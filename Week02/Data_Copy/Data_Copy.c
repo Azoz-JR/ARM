58dbfa07f33316cd40acf1b93a73c44ac0ae7066
Azoz-JR <77539943+Azoz-JR@users.noreply.github.com>
2021-09-17 01:32:43 +0200

    Create Data_Copy.c

---
+++ Week02/Data_Copy/Data_Copy.c
@@ -0,0 +1,30 @@
+#include<stdint.h>
+
+
+// this is a const data or ready only data stored in the flash memory of the MCU
+char const my_data[]="I love embedded programming";
+
+#define BASE_ADDRESS_OF_SRAM 0x20000000
+
+int main(void)
+{
+
+  for(int i=0; i< sizeof(my_data);i++)
+ {
+
+     *((uint8_t*) BASE_ADDRESS_OF_SRAM +i)=my_data[i];
+ }
+  return 0;
+}
+
+
+void NMI_Handler (void)
+{
+
+}
+
+
+void USART3_IRQHandler(void)
+{
+
+}
